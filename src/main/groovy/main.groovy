57c2faf49a893ccbe9478cfd1034df418e1bafa1
Sean Caintic <scorevi@gmail.com>
2022-01-20 20:31:42 +0800

    v1.2.2

--- src/main/groovy/main.groovy
+++ src/main/groovy/main.groovy
@@ -1,3 +1,13 @@
+class Globals {
+
+    static def username // input user
+    static def password // input password
+    static def userRole // determining user's privileges
+    static def loginState = false // if user's logged in or out
+    static def optionSelected // determining index of selected choices
+
+}
+
 // Global variables are located at 'Globals.groovy' file.
 FileChecking()
 restoLogin()
